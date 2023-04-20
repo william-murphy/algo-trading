919f23d3841bda6641be39c9fa066605fd8d5133
MiniatureScoob <cfpoirier2@gmail.com>
2023-04-20 00:49:07 -0400

    added to to readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# algo-trading
+#### algo-trading
+
+##### contributors
+Matt
+Will
+Chris
+
+##### Description
+
+A systematic trading system for analyzing financial data, engaging in market activity, and generating profit
+
+##### Prerequisites
+
+###### Database Management
+
+Some form of MySQL, PostgreSQL Oracle, SQLite, etc.
+
+###### Python imports:
+
+yfinance
+pandas
+matplotlib.pyplot
+datetime
+mysql.connector
+
+##### Installation
+
+User must rename 'credentials.example.py' to 'credentials.py' and adjust the SQL credentials
--- trader.py
+++ trader.py
@@ -1,6 +1,8 @@
 # Import package dependencies
 import yfinance as yf
 import pandas as pd
+import matplotlib.pyplot as plt
+import datetime as dt
 import mysql.connector
 
 # Import credentials.py file with sensitive info
@@ -22,9 +24,29 @@ def connect():
     else:
         cnx.close()
 
-def get_data(start_date, tickers):
-        # Get Yahoo Finance API data
-    df = pd.concat([yf.download(ticker, start=start_date, group_by="Ticker", interval='1d').assign(Ticker=ticker) for ticker in tickers], ignore_index=False)
+# Define time frame to query historical data
+start_date = '2009-03-09'
+
+# Create list of ticker symbols
+tickers = ['JPM', 'BAC', 'C', 'WFC', 'GS', 'MS']
+
+# Create get_data function to return DataFrames of error-free data for analysis
+def get_data(tickers, start_date, end_date, interval):
+
+    # get_data returns long and 2 verisons of wide data as DataFrames
+    # get_data plots histograms and line graphs of data and removes erroneous values
+    #
+    # tickers (str): desired Yahoo Finance symbol(s) for which to fetch data
+    # start_date (str): start date of query in YYYY-MM-DD format
+    # end_date (str): end date of query in YYYY-MM-DD format, defaults to today
+    # interval (str): intervals of data, options include '1d', '1wk', or '1mo'
+
+    # end_date defaults to today
+    if end_date is None:
+        end_date = dt.date.today().strftime('%Y-%m-%d')
+
+    # Get Yahoo Finance API data
+    df = pd.concat([yf.download(ticker, start=start_date, group_by='Ticker', interval='1d').assign(Ticker=ticker) for ticker in tickers], ignore_index=False)
 
     # Add 'Date' index as column and reset index of dataframe
     df = df.reset_index()
@@ -45,18 +67,55 @@ def get_data(start_date, tickers):
     df_wide_ohlcv.columns.name = None
     df_wide_ohlcv = df_wide_ohlcv.reset_index()
 
-    # Print dataframes
-    print(df_long, df_wide_ticker, df_wide_ohlcv)
+    # Calculate the original Adj Close for each instrument
+    orig_adj_close = df_wide_ohlcv.groupby('Ticker')['Adj Close'].first()
 
-# Define time frame to query historical data
-start_date = '2009-03-09'
+    # Create a new column 'Return' and set its value for each row
+    df_wide_ohlcv['Return'] = df_wide_ohlcv.apply(lambda row: row['Adj Close'] / orig_adj_close[row['Ticker']], axis=1)
 
-# Create list of ticker symbols
-tickers = ['JPM', 'BAC', 'C', 'WFC', 'GS', 'MS']
+    # Create tickers histograms
+    df_wide_ticker.hist()
 
+    # Display tickers histograms
+    plt.show()
+
+    # Create OHLCV histograms
+    df_wide_ohlcv.hist()
+
+    # Display OHLCV histograms
+    plt.show()
+
+    # Pivot the data
+    df_pivot = df_wide_ohlcv.pivot(index='Date', columns='Ticker', values='Return')
+
+    # Plot each ticker's return over time
+    for col in df_pivot.columns:
+        plt.plot(df_pivot.index, df_pivot[col], label=col)
+
+    # Set the x-axis label and title
+    plt.xlabel('Date')
+    plt.title('Returns by Ticker')
+
+    # Add a legend
+    plt.legend()
+
+    # Show the plot
+    plt.show()
+
+    return df_long, df_wide_ticker, df_wide_ohlcv
 
 def trader():
     connect()
-    get_data(start_date=start_date, tickers=tickers)
+    # Test get_data
+    df_long, df_wide_ticker, df_wide_ohlcv = get_data(tickers, '2009-03-09', None, '1d')
+
+    # Print get_data dataframes
+    print(df_long, df_wide_ticker, df_wide_ohlcv)
+
+    # Test get_data again
+    eg_long, eg_wide_ticker, eg_wide_ohlcv = get_data(['GC=F', 'SI=F'], '2011-06-15', None, '1mo')
+
+    # Print get_data dataframes again
+    print(eg_long, eg_wide_ticker, eg_wide_ohlcv)
 
     return 0
