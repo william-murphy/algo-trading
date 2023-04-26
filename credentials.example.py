81d461606c87838e16ede43eb4846cc79d31fa84
Will <wpm517@gmail.com>
2023-04-25 21:11:10 -0400

    finished basic DatabaseManager class

--- credentials.example.py
+++
@@ -1,4 +0,0 @@
-# SQL server credentials
-SQL_HOST = "localhost"
-SQL_USERNAME = "your username"
-SQL_PASSWORD = "your password"
--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from trader import Trader
 
 if __name__ == '__main__':
     trader = Trader()
-    trader.trader()
+    trader.start()
